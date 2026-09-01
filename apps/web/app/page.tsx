73e2104ac8c3477c81e9e920591532ceac6026df
mariaalexissales <mariaalexissales@gmail.com>
2026-08-31 20:18:37 -0500

    feat: render Timer on landing page with header spacer

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header";
+import Timer from "@/components/Timer";
+import Toolbar from "@mui/material/Toolbar";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Toolbar />
+      <Timer />
     </>
   );
 }
